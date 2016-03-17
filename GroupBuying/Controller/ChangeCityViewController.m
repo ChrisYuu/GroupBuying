9c3b6040ef8a34c3da98e1e70e51d52379873c59
ChrisYuu <nihaoyff@163.com>
2016-03-17 19:25:01 +0800

    添加搜索城市模块

--- GroupBuying/Controller/ChangeCityViewController.m
+++ GroupBuying/Controller/ChangeCityViewController.m
@@ -8,10 +8,18 @@
 
 #import "ChangeCityViewController.h"
 #import "CityGroupModel.h"
+#import "SearchCityResultViewController.h"
+#import "UIView+AutoLayout.h"
 
-@interface ChangeCityViewController ()<UITableViewDataSource, UITableViewDelegate>{
+@interface ChangeCityViewController ()<UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>{
     NSArray *_dataArray;
 }
+
+@property (strong, nonatomic) IBOutlet UIView *coverView;
+
+@property (strong, nonatomic) SearchCityResultViewController *resultControll;
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBarController;
+
 @end
 
 @implementation ChangeCityViewController
@@ -55,6 +63,50 @@
     cell.textLabel.text = md.cities[indexPath.row];
     return cell;
 }
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    CityGroupModel *md = [_dataArray objectAtIndex:section];
+    return md.title;
+}
+
+#pragma mark - UISearchBarDelegate
+
+-(void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    self.coverView.hidden = NO;
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+-(void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    self.coverView.hidden = YES;
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if (searchText.length){
+        self.resultControll.view.hidden = NO;
+    }else {
+        self.resultControll.view.hidden = YES;
+    }
+}
+
+#pragma mark - 创建搜索结果控制器
+- (SearchCityResultViewController *)resultControll{
+    if (!_resultControll) {
+        self.resultControll = [[SearchCityResultViewController alloc] init];
+        //将搜索结果VC添加到当前控制器中
+        [self.view addSubview:_resultControll.view];
+        //添加约束，设置搜索结果控制器的尺寸位置
+        [self.resultControll.view autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeTop];
+        //让resultContoller的顶部 贴着搜索框的底部 不遮盖搜索框
+        [self.resultControll.view autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.searchBarController];
+        
+    }
+    return _resultControll;
+}
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ GroupBuying/Controller/SearchCityResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchCityResultViewController.h
+//  GroupBuying
+//
+//  Created by Chris on 16/3/17.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchCityResultViewController : UITableViewController
+
+@end
